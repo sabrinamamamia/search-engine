07141d99f362d7e6e24049f2b2723c2a7051c280
Sabrina <sabrinama10@gmail.com>
2018-04-22 23:11:51 -0400

    Completed p3

--- p3/cluster.py
+++ p3/cluster.py
@@ -4,8 +4,9 @@ import random
 import math
 import sys
 import copy
-from sklearn.metrics.pairwise import cosine_similarity
-
+import sys
+import os
+from time import time
 
 class Cluster:
 	def __init__(self, centroids, clusters):
@@ -36,15 +37,14 @@ def getClusterID(docID, clusters):
 	return None
 
 def getDistance(vector1, vector2):
-	"""Calculates Euclidean distance (the square root 
-		of tf*idf difference across all dimensions squared)
-		between two vectors.
+	"""Calculates cosine similarity between vectors.
 	Args:
 		vector1: vector 1
 		vector2: vector 2
 	Returns:
 		distance: float representing euclidean distance 
 	"""
+	# Euclidean distance - did not work for me
 	# distance = np.linalg.norm(vector1-vector2)
 	# # summation = 0
 	# # for idx, val in enumerate(centroid):
@@ -112,13 +112,8 @@ def KMeans(K, docs, docTermMatrix):
 	i = 0
 	finalSSE = 100000
 	newSSE = finalSSE - 1
-	# TODO: Why does SSE increase after i + 1 iterations
 	while i < MAX_ITERATIONS and newSSE < finalSSE:
-	# while i < MAX_ITERATIONS:
-		# print("Itr: " + str(i))
 		finalSSE = newSSE
-		finalClusters = clusters 		#Shallow copy
-		finalCentroids = centroids
 		if type(docs) == set:
 			docs = {a:a for a in docs}
 
@@ -128,36 +123,29 @@ def KMeans(K, docs, docTermMatrix):
 			distances = []
 			for k in centroids:
 				distances.append(getDistance(docTermMatrix[docs[d]], k))
-			# print(distances)
 			# Reassign vector to closest cluster
 			oldk = getClusterID(docs[d], clusters)
-			newk = distances.index(min(distances))
+			newk = distances.index(max(distances))
 			if oldk != None and oldk != newk:
 				clusters[oldk].remove(docs[d])
-				# print("moving doc to new clustre")
 			if oldk != newk:
 				clusters[newk].add(docs[d])
-			# print([len(c) for c in clusters])
 		SSE = 0
-		k_SSE = []
+		# k_SSE = []
 		# Recompute centroid and get SSE
 		for k in range(K):
 			centroids[k] = recomputeCentroid(clusters[k], docTermMatrix)
-			# print("change between centroids")
-			# print(getDistance(old, centroids[k]))
 			SSE += getSSE(centroids[k], clusters[k], docTermMatrix)
-			k_SSE.append(getSSE(centroids[k], clusters[k], docTermMatrix))
-		# print("SSE of ea cluster: " + str(k_SSE))
-		# print("SSE: " + str(SSE))
+			# k_SSE.append(getSSE(centroids[k], clusters[k], docTermMatrix))
 		newSSE = SSE 
 		i +=1
 	
-	FINAL_k_SSE = []
-	for k in range(K):
-		FINAL_k_SSE.append(round(getSSE(centroids[k], clusters[k], docTermMatrix),2))
-	print("Cluster sizes:" + str([len(c) for c in clusters]))
-	print("SSE of ea cluster: " + str(FINAL_k_SSE))
-	print("Final SSE: " + str(round(finalSSE,2)))
+	# FINAL_k_SSE = []
+	# for k in range(K):
+		# FINAL_k_SSE.append(round(getSSE(centroids[k], clusters[k], docTermMatrix),2))
+	# print("Cluster sizes:" + str([len(c) for c in clusters]))
+	# print("SSE of ea cluster: " + str(FINAL_k_SSE))
+	# print("SSE: " + str(round(finalSSE,2)))
 	return Cluster(centroids, clusters)
 
 def processQuery(query, centroids, docTermMatrix):
@@ -167,45 +155,81 @@ def processQuery(query, centroids, docTermMatrix):
 		query: query vector
 		centroids: list of cluster centroids
 	Returns:
-		maxSCs: list of indices of centroid with highest SC for each query
+		SCs: list of [index, distance] sorted in ascending order
 	"""
 	SCs = []
-	for centroid in centroids:
-		SCs.append(getDistance(query, centroid))
+	for idx, centroid in enumerate(centroids):
+		SCs.append([idx, getDistance(query, centroid)])
 	# Highest SC = smallest distance between query and centroid
-	print(SCs)
-	return SCs.index(min(SCs))
+	# print(sorted(SCs, key = lambda x: x[1], reverse=True))
+	return sorted(SCs, key = lambda x: x[1], reverse=True)
+
+def generateScores(clusters, query, selected, SCs, queryIDs, docs, docTermMatrix):
+	# Get SC between query and documents in selected cluster
+	scores = []	
+	for idx in clusters[selected]: 
+		scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
+	# Search other clusters if selected cluster has < 100 documents
+	while len(scores) < 100 and len(SCs) > 0:
+		# print("Original cluster too small, searching other clusters")
+		selected = SCs.pop(0)[0]
+		for idx in clusters[selected]: 
+			scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
+	sortedScores = sorted(scores, key = lambda x: x[1], reverse=True)
+	for idx, score in enumerate(sortedScores[:100]):
+		line = str(queryIDs[qidx]) + " 0 " + score[0] + " " + str(idx) + " " + str(score[1]) + " COSINE"
+		resultsFile.write(line + "\n")
 
 def analyzeCluster(clusters, centroids, docTermMatrix):
-	"""Calculates size, intra-class similarity (avg distance between vectors in cluster),
+	"""Calculates size, intra-class similarity (avg distance betwegit vectors in cluster),
 	 and inter-class similarity of clusters (avg distance between clusters)
 	Args:
 		clusters: list of clusters
 	"""
+	# Interclass similarity
 	interClass = 0
-	intraClass = []	
-	sizes = []
 	n = len(clusters)
+	for idx, c in enumerate(clusters):
+		for idx1, c1, in enumerate(clusters):
+			if idx1 > idx:
+				interClass += getDistance(centroids[idx], centroids[idx1])
+				# print("c" + str(idx) + " <-> c" + 
+				# 	str(idx1) + " " + str(round(getDistance(centroids[idx], centroids[idx1]),2)))
+	print(round(interClass / ((n * (n-1)) / 2),2))
 
-	for idx1, c1 in enumerate(clusters): 
-		# To determine interclass, compare every clusters centroids with every other
-		# cluster then divide by n(n - 1) / 2
-		for idx2, c2 in enumerate(clusters):
-			interClass += getDistance(centroids[idx1], centroids[idx2])
-		# Intraclass: compare every document with centroid and divide by total # of doc ??
-		intra = 0
-		for doc in cluster:
-			intra += getDistance(centroids[idx], docTermMatrix[doc])
-		intraClass.append(intra/len(cluster))
-	interclass = interclass / (n(n-1) / 2)
+	# Intraclass similarity
+	# Test only on cluster 0
+	intraClass = 0
+	for doc in clusters[0]:
+		# print(getDistance(centroids[0], docTermMatrix[doc]))
+		intraClass += getDistance(centroids[0], docTermMatrix[doc])
+	print(round(intraClass / len(clusters[0]),2))
+
+	# print(getDistance(centroids[0], docTermMatrix[doc]))
 
 def main():
-	DOCTERM_MATRIX = "data/docterm-matrix.txt"
-	QUERY_MATRIX = "data/query-matrix.txt"
-	DIMENSIONS = "data/dimensions.txt"
-	RESULTS = "data/test-results.txt"
+	start_time = time()
+	inputPath = sys.argv[1]
+	resultsPath = sys.argv[2]
+	K = int(sys.argv[3])
+
+	# Creates result output directory if necessary
+	resultsPath  = resultsPath.split("/")
+	resultsPath.pop(0) if resultsPath[0] == "." else resultsPath
+	resultsDir = resultsPath[0] 
+	if not os.path.exists(resultsDir):
+		os.makedirs(resultsDir)
+
+	if inputPath[-1] != "/":
+		inputPath += "/"  
+	if resultsDir[-1] != "/":
+		resultsDir += "/" 
+
+	DOCTERM_MATRIX = inputPath + "docterm-matrix.txt"
+	QUERY_MATRIX = inputPath + "query-matrix.txt"
+	DIMENSIONS = inputPath + "dimensions.txt"
+	RESULTS = resultsDir + "results.txt"
 	resultsFile = open(RESULTS, "a+")
-	K = 8
 
 	docTermDict = eval(open(DOCTERM_MATRIX, 'r').read())
 	queryDict = eval(open(QUERY_MATRIX, 'r').read())
@@ -216,77 +240,52 @@ def main():
 	docTermMatrix = np.array([docTermDict[i] for i in docs])
 	queryMatrix = np.array([queryDict[i] for i in queryIDs])
 
-	# for qidx, query in enumerate(queryMatrix):
-	# 	scores = [] 
-	# 	for i, doc in enumerate(docTermMatrix):
-	# 		scores.append([docs[i], getDistance(query, doc)])
-	# 	sortedScores = sorted(scores, key = lambda x: x[1], reverse=True)
-	# 	for idx, score in enumerate(sortedScores[:100]):
-	# 		line = str(queryIDs[qidx]) + " 0 " + score[0] + " " + str(idx) + " " + str(score[1]) + " EUCLID"
-	# 		resultsFile.write(line + "\n")
-
-	# Q = np.array([0,0,0,0,0,.176,0,0,.477,0,.176])
-	# D = np.array([0,0,.477,0,.477,.176,0,0,0,.176,0])
-
-	# print(terms.index("domestic"))
-	# print(queryMatrix[0])
-	# print(docTermMatrix[docsDict["FR940617-2-00223"]])
-	# print(getDistance(queryMatrix[0],docTermMatrix[docsDict["FR940617-2-00223"]]))
-
-	# print(len(terms))
-
-	# for idx, score in enumerate(sortedScores):
-	# 	line = str(queryIDs[qidx]) + " 0 " + score[0] + " " + str(idx) + " " + str(score[1]) + " EUCLID"
-	# 	resultsFile.write(line + "\n")
-
 	# Cluster document collection
 	clusterComponents = KMeans(K, docsDict, docTermMatrix)
 	centroids = clusterComponents.centroids
 	clusters = clusterComponents.clusters
 
-	for c in clusters:
-		if docsDict["FR940617-2-00223"] in c:
-			print ("in cluster " + str(clusters.index(c)))
-
-	# highestSC = processQuery(queryMatrix[0], centroids, docTermMatrix)
-	# KMeans(K, clusters[highestSC], docTermMatrix)
-	# print(queryMatrix[0])
-	highestSC = processQuery(queryMatrix[0], centroids, docTermMatrix)
-	print("highest SC cluster" + str(highestSC))
-
-	# for docid in clusters[highestSC]:
-		# print(docs[docid])
-	# print(docsDict["FR940617-2-00223"])
-	print(clusters[highestSC])
-
-	scores = []
-	for idx in clusters[highestSC]: 
-		print(getDistance(docTermMatrix[idx], queryMatrix[0]))
-		scores.append([docs[idx], getDistance(docTermMatrix[idx], queryMatrix[0])])
-	# print(scores)
-	sortedScores = sorted(scores, key = lambda x: x[1], reverse=True)
-	for idx, score in enumerate(sortedScores[:100]):
-		line = str("265 0 " + score[0] + " " + str(idx) + " " + str(score[1]) + " COSINE")
-		resultsFile.write(line + "\n")
-
 	# Perform query processing on clustered collection
-	# Calculate the SC between the query vector and each cluster centroid 
-	# for qidx, query in enumerate(queryMatrix):
-	# 	highestSC = processQuery(query, centroids, docTermMatrix)
-		# print(len(clusters[highestSC]))
-		# while len(clusters[highestSC]) > 100:
-		# 	clusterComponents = KMeans(K - 3, clusters[highestSC], docTermMatrix)
-		# 	centroids = clusterComponents.centroids
-		# 	clusters = clusterComponents.clusters
-		# 	highestSC = processQuery(query, centroids, docTermMatrix)
-		# scores = []
-		# for idx in clusters[highestSC]: 
-		# 	scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
-		# sortedScores = sorted(scores, key = lambda x: x[1], reverse=True)
-		# # print(str([s for s in sortedScores]))
-		# for idx, score in enumerate(sortedScores):
-		# 	line = str(queryIDs[qidx]) + " 0 " + score[0] + " " + str(idx) + " " + str(score[1]) + "COSINE"
-		# 	resultsFile.write(line + "\n")
+	for qidx, query in enumerate(queryMatrix):
+		if qidx != 21:	# Query ID 374 returns 0 results, skip
+			# Calculate the SC between the query vector and each cluster centroid 
+			SCs = processQuery(query, centroids, docTermMatrix)
+			selected = SCs.pop(0)[0]
+			# Recluster if selected cluster has > 100 documents 
+			if len(clusters[selected]) > 100: 
+				recluster = True
+				# Make size of K 75% of orginal so clusters are not too small
+				reClusterComponents = KMeans(int(K * .75), clusters[selected], docTermMatrix)
+				centroids1 = reClusterComponents.centroids
+				clusters1 = reClusterComponents.clusters
+				SCs = processQuery(query, centroids1, docTermMatrix)
+				selected = SCs.pop(0)[0]
+
+			else: 
+				recluster = False
+			scores = []	
+			if recluster == False:
+				for idx in clusters[selected]: 
+					scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
+			elif recluster == True:
+				for idx in clusters1[selected]: 
+					scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
+			
+			# Search other clusters if selected cluster has < 100 documents
+			while len(scores) < 100 and len(SCs) > 0:
+				selected = SCs.pop(0)[0]
+				if recluster == False:
+					for idx in clusters[selected]: 
+						scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
+				elif recluster == True:
+					for idx in clusters1[selected]: 
+						scores.append([docs[idx], getDistance(docTermMatrix[idx], query)])
+			sortedScores = sorted(scores, key = lambda x: x[1], reverse=True)
+			for idx, score in enumerate(sortedScores[:100]):
+				line = str(queryIDs[qidx]) + " 0 " + score[0] + " " + str(idx) + " " + str(score[1]) + " COSINE"
+				resultsFile.write(line + "\n")
+	end_time = time()
+	print('Time:   {:.3f} s'.format(end_time - start_time))
 
 if __name__== "__main__":
 	main()
--- p3/preprocess.py
+++ p3/preprocess.py
@@ -4,87 +4,32 @@
 import ast
 import json
 import preprocess_query 
+import sys
+import os
+from time import time
 
-def indexToMatrix():
-	"""Converts inverted index into document-term matrix and query matrix. 
+def indexToMatrix(inputPath, resultsPath, numDimensions):
+	"""Converts single term inverted index into document-term matrix and query matrix. 
+	Args:
+		inputPath: data input directory path
+		resultsPath: directory where result files will go
 	Returns:
 	  doc-term text file
 	  query matrix text file
+	  dimensions text file
 	"""
 	# inverted index format: term df idf cf [posting list])
-	INDEX = "data/single.txt"
-	# INDEX = "data/stem.txt"
+	INDEX = inputPath + "single.txt"
 	# document format: docID tf tf_idf
-	DOC = "data/single-docLength.txt"
-	# DOC = "data/stem-docLength.txt"
-	LEXICON = "data/lexicon.txt"
-	QUERY = "data/queries.txt"
-	QUERY_MATRIX = "data/query-matrix.txt"
-	DOCTERM_MATRIX = "data/docterm-matrix.txt"
-	# DOCTERM_MATRIX = "data/stem-docterm-matrix.txt"
-	DIMENSIONS = "data/dimensions.txt"
+	DOC = inputPath + "single-docLength.txt"
+	LEXICON = inputPath + "lexicon.txt"
+	QUERY = inputPath + "queries.txt"
+	QUERY_MATRIX = resultsPath + "query-matrix.txt"
+	DOCTERM_MATRIX = resultsPath + "docterm-matrix.txt"
+	DIMENSIONS = resultsPath + "dimensions.txt"
+
 	with open(INDEX) as indexFile, open(DOC) as docFile, open(QUERY) as qFile:
-		
-		# # Convert index to list and filter top 5000 terms with highest idf
-		# index = []
-		# for line in indexFile: 
-		# 	term = line.split("\t")[0]
-		# 	idf = float(line.split("\t")[1].split(" ")[1])
-		# 	pList = line.split("\t")[2].replace("\n", "")
-		# 	index.append([term, idf, pList])
-		
-		# # Sort by idf ascending 
-		# sortedIndex = sorted(index, key=lambda x: x[1], reverse=False)
-
-		# # Filter out idf > 1 
-		# # sortedIndex1 = sorted(sortedIndex, key=lambda x: x[1] >= 1, reverse=False)
-		# sortedIndex1 = list(filter(lambda x: x[1] >= 1, sortedIndex))
-
-		# # Filter top 5000 terms and sort alphabetically
-		# filteredIndex = sorted(sortedIndex1[:5000], key=lambda x: x[0])
-
-		# # Create empty doc-term dict to be the doc-term matrix
-		# docTermMatrix = {line.split(" ")[0]: [0] * 5000 for line in docFile}
-		# # Create (term: index) dict so weights placed in correct index in matrix
-		# termIndex = {k[0]: v for v, k in enumerate(filteredIndex)}
-		# indexDict = {}	# (term: idf) dict
-		# # Place tfidf weights in doc-term matrix
-		# for elem in filteredIndex:
-		# 	term = elem[0]
-		# 	idf = elem[1]
-		# 	pList = ast.literal_eval(elem[2])
-		# 	indexDict[term] = idf
-		# 	for d in pList:
-		# 		docID = d[0]
-		# 		tf = int(d[1])
-		# 		termIdx = termIndex[term]
-		# 		docTermMatrix[docID][termIdx] = tf * idf
-
-		# # Preprocess queries 
-		# queryNums = []
-		# queries = []
-		# for line in qFile.readlines():
-		# 	if line.startswith("<num>"):
-		# 		num = line.replace("<num> Number: ", "").replace("\n", "").rstrip()
-		# 		queryNums.append(num)
-		# 	elif line.startswith("<title>"):
-		# 		num = line.replace("<title> Topic: ", "").replace("\n", "").rstrip()
-		# 		queries.append(num)
-		# queryMatrix = {num: [0] * 5000 for num in queryNums}
-		# queryTerms = {}
-		# for qIdx, q in enumerate(queries):
-		# 	query = preprocess_query.parse(q, "single")	# (term: tf) dict 
-		# 	queryID = queryNums[qIdx]
-		# 	for term in query:
-		# 		queryTerms.add(term)
-		# 		if term in termIndex:
-		# 			termIdx = termIndex[term]
-		# 			tf = query[term]
-		# 			idf = indexDict[term]
-		# 			queryMatrix[queryID][termIdx] = tf * idf
-		# print(queryMatrix)
-
-		# Convert index to list and filter top 5000 terms with highest idf
+		# Convert index to list and filter top n terms with highest idf
 		index = []
 		indexDict = {}
 		for line in indexFile: 
@@ -119,8 +64,8 @@ def indexToMatrix():
 		# sortedIndex1 = sorted(sortedIndex, key=lambda x: x[1] >= 1, reverse=False)
 		sortedIndex1 = list(filter(lambda x: x[1] >= 1, sortedIndex))
 
-		# Filter top 5000 terms and sort alphabetically
-		filteredIndex = sorted(sortedIndex1[:5000])
+		# Filter top n terms and sort alphabetically
+		filteredIndex = sorted(sortedIndex1[:numDimensions])
 		filteredIndexSet = {s[0] for s in filteredIndex}
 
 		for term in queryTerms:
@@ -160,4 +105,28 @@ def indexToMatrix():
 		f4.write(json.dumps(queryMatrix))
 		f5.write(str(list(termIndex.keys())))
 
-indexToMatrix()
+
+def main():
+
+	# python3 preprocess.py [input-directory-path] [results-directory-path] [num-dimensions]
+	inputPath = sys.argv[1]
+	resultsPath = sys.argv[2]
+	numDimensions = int(sys.argv[3])
+
+	# Creates result output directory if necessary
+	resultsPath  = resultsPath.split("/")
+	resultsPath.pop(0) if resultsPath[0] == "." else resultsPath
+	resultsDir = resultsPath[0] 
+	if not os.path.exists(resultsDir):
+		os.makedirs(resultsDir)
+
+	if inputPath[-1] != "/":
+		inputPath += "/"  
+	if resultsDir[-1] != "/":
+		resultsDir += "/" 
+
+	indexToMatrix(inputPath, resultsDir, numDimensions)
+
+if __name__== "__main__":
+	main()
+
